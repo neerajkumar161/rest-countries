07457164ebcfe9df35277766eecb9069e50032ea
neerajkumar161 <ennkay161@gmail.com>
2023-10-01 00:14:06 +0530

    fix: filtered countries shallow copy fixed

--- src/app/countries/controller.ts
+++ src/app/countries/controller.ts
@@ -30,11 +30,10 @@ export class CountryController implements ICountryController {
           if (country.languages[language as string]) {
             filteredCountries.push(country)
           }
+        } else {
+          // if no filter is provided, we will send entire response
+          filteredCountries = JSON.parse(JSON.stringify(countries))
         }
-      }
-
-      if (!area && !population && !language) {
-        filteredCountries = countries
       }
 
       //After fetching all records, we will sort the order and apply pagination
